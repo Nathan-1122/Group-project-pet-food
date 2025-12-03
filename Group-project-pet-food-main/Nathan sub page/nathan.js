9903f1954059c3b148fcbd788b5359deea27b088
Nathan Weir <G00484194@atu.ie>
2025-12-03 12:45:15 +0000

    purchase page/button and fixes

--- Group-project-pet-food-main/Nathan sub page/nathan.js
+++ Group-project-pet-food-main/Nathan sub page/nathan.js
@@ -1,4 +1,16 @@
+function purchase()
+{
+  window.location.replace("/nathanpurchase.html");
+}
 
+function buy(){
+    alert("congratulations on purchasing NAI petfood's \"Smudge Style cat food\"");
+    window.location.replace("index.html");
+}
+
+function cancel(){
+  window.location.replace("index.html");
+}
 
 
 
